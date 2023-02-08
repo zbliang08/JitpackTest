6aac2f5c2579e43d6852f2d130c2beb22102cb4e
zbliang <liangzhibo@xa.com>
2023-02-08 11:47:02 +0800

    save

---
+++ app/src/main/java/com/xa/jitpacktest/MyTest3.kt
@@ -0,0 +1,11 @@
+package com.xa.jitpacktest
+
+import com.xa.mylibrary.Fuck
+
+class MyTest3 {
+    fun show(){
+        Fuck().fangfang()
+        Fuck().fangfang()
+        Fuck().fangfang()
+    }
+}
---
+++ mylibrary/src/main/java/com/xa/mylibrary/Fuck3.kt
@@ -0,0 +1,9 @@
+package com.xa.mylibrary
+
+import android.util.Log
+
+class Fuck3 {
+    fun fangfang(){
+        Log.v("lzb","fuck3")
+    }
+}
